d48335b085e7629aeecfd35bb45bd33f8f0e2d8a
DamColson <damien.colson@gmail.com>
2019-04-08 11:50:00 +0200

    partie 3 en cours

---
+++ Exercice.js
@@ -0,0 +1,15 @@
+// Exercice 1 partie 3
+//
+// function borderImage(img){
+//   img.style.border = 'solid 1px red';
+// }
+//
+// function borderOut(img){
+//   img.style.border = "";
+// }
+
+// Exercice 2 partie 3
+
+function hideText(){
+
+}
