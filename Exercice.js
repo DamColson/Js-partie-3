e1d30acff4d63a49bb24033dcc3badda391c6a67
DamColson <damien.colson@gmail.com>
2019-04-08 14:16:43 +0200

    partie 3 terminée

--- Exercice.js
+++ Exercice.js
@@ -10,6 +10,52 @@
 
 // Exercice 2 partie 3
 
-function hideText(){
+// var textExo = document.getElementById('text');
+// var hidePar = document.getElementById('hide');
+// var showPar = document.getElementById('show');
+//
+// function hideText(){
+//   textExo.style.display = 'none';
+// }
+//
+// function showText(){
+//   textExo.style.display = '';
+// }
+//
+// hidePar.addEventListener('click',function(){
+//   hideText();
+// });
+// showPar.addEventListener('click',function(){
+//   showText();
+// });
 
-}
+// Exercice 3 partie 3
+
+// document.getElementsByClassName('green')[0].addEventListener('click', function() {
+//   document.getElementById('text').style.color = 'green';
+// });
+// document.getElementsByClassName('red')[0].addEventListener('click', function() {
+//   document.getElementById('text').style.color = 'red';
+// });
+// document.getElementsByClassName('blue')[0].addEventListener('click', function() {
+//   document.getElementById('text').style.color = 'blue';
+// });
+
+// Exercice 3 partie 3 solution 2
+//
+// function change(elem){
+//     document.getElementById('text').style = 'color :'+elem.classList.item(1)+';';
+//   }
+
+// Exercice 4 partie 3
+
+// document.getElementById('verif').addEventListener('click',function passVerif(){
+//   if(document.getElementById('password').value == document.getElementById('confirmPassword').value){
+//     document.getElementById('password').style.border = 'solid 1px green';
+//     document.getElementById('confirmPassword').style.border = 'solid 1px green';
+//   } else {
+//     document.getElementById('password').style.border = 'solid 1px red';
+//     document.getElementById('confirmPassword').style.border = 'solid 1px red';
+//   }
+//
+// });
